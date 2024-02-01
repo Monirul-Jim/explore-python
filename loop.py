afc99ad44c2933a6068c64ee6f9d403a215fa021
unknown <Monirul-Jim>
2024-02-01 22:02:12 +0600

    dsjfhds

---
+++ loop.py
@@ -0,0 +1,12 @@
+# learn python loop use range
+# for i in range(10, 3, -2):
+#     print(i)
+sum = 0
+for i in range(1, 11):
+    sum = sum+i
+print(sum)
+# break keyword
+for i in range(10):
+    if i == 5:
+        break
+    print(i)
