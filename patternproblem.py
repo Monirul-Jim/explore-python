c4ebb4b07c604bf9433a42fd47ef9566e72752ad
unknown <Monirul-Jim>
2024-02-02 12:46:17 +0600

    jahhdf

---
+++ patternproblem.py
@@ -0,0 +1,11 @@
+# for row in range(6):
+#     for col in range(row+1):
+#         print("#", end=" ")
+#     print()
+# 2 character loop
+# small letter starts with 97
+# big letter starts with 65
+for row in range(7):
+    for col in range(row+1):
+        print(chr(97+row), end=" ")
+    print()
