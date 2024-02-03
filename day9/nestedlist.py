3d5e4b2d5d691552e43a9634b0efa4ad4619e24e
unknown <Monirul-Jim>
2024-02-03 22:56:11 +0600

    learn swap

--- day9/nestedlist.py
+++ day9/nestedlist.py
@@ -1,1 +1,12 @@
+# transpose matrix conversation
 matrix = [[2, 3], [4, 5], [6, 7], [8, 9]]
+a = []
+for row in range(2):
+    b = []
+    for col in matrix:
+        b.append(col[row])
+    a.append(b)
+print(a)
+# result = [[col[row] for col in matrix] for row in range(2)]
+result = [[col[row] for col in matrix] for row in range(2)]
+print(result, "result")
